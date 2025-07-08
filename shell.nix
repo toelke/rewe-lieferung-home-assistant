fa299cf0d0f66d77c6afe0f5933336a5cfccb9e9
Philipp Riederer <philipp@riederer.email>
2025-07-08 14:58:31 +0200

    fix shell.nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
 let pkgs = import <nixpkgs> { };
 in pkgs.mkShell {
-  buildInputs = [ (pkgs.python311.withPackages (p: with p; [ pip arrow ics requests cachetools structlog cherrypy ])) ];
+  buildInputs = [ (pkgs.python3.withPackages (p: with p; [ pip arrow ics requests cachetools structlog cherrypy ])) ];
   shellHook = ''
     # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
     # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
     export PIP_PREFIX=$(pwd)/_build/pip_packages
-    export PYTHONPATH="$PIP_PREFIX/${pkgs.python311.sitePackages}:$PYTHONPATH"
+    export PYTHONPATH="$PIP_PREFIX/${pkgs.python1.sitePackages}:$PYTHONPATH"
     export PATH="$PIP_PREFIX/bin:$PATH"
     unset SOURCE_DATE_EPOCH
   '';
